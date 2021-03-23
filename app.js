9948c148d3824fa52a293eb4e9d9e4e37d7fda63
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-03-24 01:31:43 +0200

    basic GET /api/tickets

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 
+const Ticket = require("./models/Ticket");
+
 app.use(express.static("client/build"));
+app.use(express.json());
+
+// GET route to /api/tickets - returns an array of all tickets in the collection tickets in database.
+app.get("/api/tickets", (req, res) => {
+	Ticket.find({}).then((tickets) => {
+		res.json(tickets);
+	});
+});
 
 module.exports = app;
