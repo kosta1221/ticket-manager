4c103e8340edd97060780a4947b0f43c2ee2f6b2
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-03-24 01:34:27 +0200

    add morgan logging

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
-
+const morgan = require("morgan");
 const Ticket = require("./models/Ticket");
+
+morgan.token("reqbody", (req) => {
+	return JSON.stringify(req.body);
+});
 
 app.use(express.static("client/build"));
 app.use(express.json());
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :reqbody"));
 
 // GET route to /api/tickets - returns an array of all tickets in the collection tickets in database.
 app.get("/api/tickets", (req, res) => {
