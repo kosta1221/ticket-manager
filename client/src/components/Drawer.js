2ddc7622d1ef7d474cde1ee29a34ba6f096d9a0e
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-03-28 23:46:07 +0300

    Add categories to drawer (non- functional)

--- client/src/components/Drawer.js
+++ client/src/components/Drawer.js
@@ -3,6 +3,17 @@ import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
 import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
 
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import CheckCircle from "@material-ui/icons/CheckCircle";
+import MarkAsUndoneIcon from "@material-ui/icons/HighlightOff";
+import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
+
 const useStyles = makeStyles({
 	left: {
 		width: 250,
@@ -26,7 +37,33 @@ function Drawer({ drawerShowed, setDrawerShowed }) {
 			role="presentation"
 			onClick={toggleDrawer(false)}
 			onKeyDown={toggleDrawer(false)}
-		></div>
+		>
+			<List>
+				{["All Tickets", "Done", "Undone"].map((text, index) => (
+					<ListItem button key={text}>
+						<ListItemIcon>
+							{index % 3 === 0 ? (
+								<InboxIcon />
+							) : index % 2 === 0 ? (
+								<MarkAsUndoneIcon />
+							) : (
+								<CheckCircle />
+							)}
+						</ListItemIcon>
+						<ListItemText primary={text} />
+					</ListItem>
+				))}
+			</List>
+			<Divider />
+			<List>
+				{["Hidden Tickets"].map((text, index) => (
+					<ListItem button key={text}>
+						<ListItemIcon>{<VisibilityOffIcon />}</ListItemIcon>
+						<ListItemText primary={text} />
+					</ListItem>
+				))}
+			</List>
+		</div>
 	);
 
 	return (
