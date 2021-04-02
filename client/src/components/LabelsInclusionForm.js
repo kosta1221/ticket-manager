7a3b6ead862ddbb39e5f1fa78889787437e702a8
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-04-02 19:59:52 +0300

    can toggle on/off unique labels (ugly css, non func)

--- client/src/components/LabelsInclusionForm.js
+++ client/src/components/LabelsInclusionForm.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
-import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import Chip from "@material-ui/core/Chip";
 
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
@@ -19,17 +18,31 @@ import { makeStyles } from "@material-ui/core/styles";
 
 import LabelIcon from "@material-ui/icons/LocalOfferOutlined";
 
+import ToggleButton from "@material-ui/lab/ToggleButton";
+import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
+
 const useStyles = makeStyles((theme) => ({
-	formControl: {
-		margin: theme.spacing(1),
+	label: {
+		margin: theme.spacing(0.3),
 	},
 }));
 
-function SortTicketsForm({ tickets, labelFormOpen, setLabelFormOpen }) {
+function SortTicketsForm({
+	tickets,
+	labelFormOpen,
+	setLabelFormOpen,
+	allLabels,
+	currentLabels,
+	setCurrentLabels,
+}) {
 	const classes = useStyles();
 
 	const handleClose = () => {
 		setLabelFormOpen(false);
+	};
+
+	const handleLabelsChange = (event, newLabels) => {
+		setCurrentLabels(newLabels);
 	};
 
 	return (
@@ -46,6 +59,30 @@ function SortTicketsForm({ tickets, labelFormOpen, setLabelFormOpen }) {
 						Please select the labels/tags of the tickets that you'd like to include. If none are
 						chosen, all tickets will be shown.
 					</DialogContentText>
+					<ToggleButtonGroup
+						value={currentLabels}
+						onChange={handleLabelsChange}
+						aria-label="text formatting"
+					>
+						{allLabels.map((label, i) => (
+							<ToggleButton
+								className={classes.label}
+								key={`toggle-button-label-${i}`}
+								value={label}
+								aria-label={label}
+								onClick={() => {}}
+								color="primary"
+							>
+								<Chip
+									key={`label-${i}`}
+									className="label"
+									label={label}
+									onClick={() => {}}
+									color="primary"
+								></Chip>
+							</ToggleButton>
+						))}
+					</ToggleButtonGroup>
 				</DialogContent>
 			</Dialog>
 		</div>
