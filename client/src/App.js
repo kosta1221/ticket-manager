630b9e8c2d7ea5b837f1c7cd58b8e7306b06e43a
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-04-08 14:37:30 +0300

    Revert "useless"
    This reverts commit 1c56284647b7e0d1790bf1bb5059123c8a7a4f0b.

--- client/src/App.js
+++ client/src/App.js
@@ -248,9 +248,6 @@ function App() {
 	return (
 		<ThemeProvider theme={theme}>
 			<div className={`${classes.App} App`}>
-				<div>
-					<p>adfgdfgdfg</p>
-				</div>
 				<SearchAppBar
 					setInput={setInput}
 					searchBy={searchBy}
